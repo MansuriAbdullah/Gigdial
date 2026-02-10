11151ae9566bd4047ce7086c652ce12d3290d174
MansuriAbdullah <mansuri123mansuri123@gmail.com>
2026-02-10 18:38:25 +0530

    fix: Support Bearer token in auth middleware

--- gigdial/gitdial-backend/middleware/authMiddleware.js
+++ gigdial/gitdial-backend/middleware/authMiddleware.js
@@ -6,12 +6,17 @@ const protect = async (req, res, next) => {
 
     token = req.cookies.jwt;
 
+    if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.userId).select('-password');
             next();
         } catch (error) {
+            console.error(error);
             res.status(401);
             throw new Error('Not authorized, token failed');
         }
